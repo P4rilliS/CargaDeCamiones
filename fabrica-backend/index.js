fd9409c2f67807673f865b9ab54916341c3a8acd
Sergio Parilli <sergioparilli06@gmail.com>
2026-04-03 17:12:59 -0400

    a

--- fabrica-backend/index.js
+++ fabrica-backend/index.js
@@ -101,6 +101,6 @@ app.get('/api/pedidos', async (req, res) => {
 
 // --- ARRANCAR EL SERVIDOR ---
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor listo en el puerto ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`)
 });
